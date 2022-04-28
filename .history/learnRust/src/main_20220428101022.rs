766bcafd5ab4d27fd91fc0d6de83fc2425db53f3
superai2-970 <jaroensak.yod@gmail.com>
2022-04-28 10:11:56 +0700

    28/4

---
+++ .history/learnRust/src/main_20220428101022.rs
@@ -0,0 +1,13 @@
+#![allow(unused_variables)]
+#![allow(unused_assignments)]
+#![allow(dead_code)]
+#![allow(while_true)]
+#![allow(unreachable_code)]
+#![allow(unused_mut)]
+#![allow(unused_imports)]
+
+fn main() {
+    println!("Hello, world!");
+    let x: i32 =10;
+    println!("{}", x);
+}
