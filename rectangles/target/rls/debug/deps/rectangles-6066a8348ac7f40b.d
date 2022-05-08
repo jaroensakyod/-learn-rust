5268ef05e830ab1cac1db8c488d7d21197bcb51c
superai2-970 <jaroensak.yod@gmail.com>
2022-05-08 11:23:34 +0700

    rectangles
    Signed-off-by: superai2-970 <jaroensak.yod@gmail.com>

---
+++ rectangles/target/rls/debug/deps/rectangles-6066a8348ac7f40b.d
@@ -0,0 +1,5 @@
+c:\Users\ASUS\Desktop\learn rust\rectangles\target\rls\debug\deps\rectangles-6066a8348ac7f40b.rmeta: src\main.rs
+
+c:\Users\ASUS\Desktop\learn rust\rectangles\target\rls\debug\deps\rectangles-6066a8348ac7f40b.d: src\main.rs
+
+src\main.rs:
---
+++ rectangles/target/rls/debug/deps/rectangles-937cd89110303d07.d
@@ -0,0 +1,5 @@
+c:\Users\ASUS\Desktop\learn rust\rectangles\target\rls\debug\deps\rectangles-937cd89110303d07.rmeta: src\main.rs
+
+c:\Users\ASUS\Desktop\learn rust\rectangles\target\rls\debug\deps\rectangles-937cd89110303d07.d: src\main.rs
+
+src\main.rs:
